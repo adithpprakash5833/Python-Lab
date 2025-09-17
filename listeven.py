e589b8c828c52ece22dd0e74d176a15355141bab
adithpprakash5833 <adithpprakash5833@gmail.com>
2025-09-17 15:49:12 +0530

    Add files via upload

---
+++ listeven.py
@@ -0,0 +1,6 @@
+l1=int(input("Enter numbers seperated by commas:")).split(',')
+even_number=[]
+for i in number:
+    if i%2==0:
+        even_number.append(i)
+print(even_number)
---
+++ oddlist.py
@@ -0,0 +1,6 @@
+number=[1,2,3,4,5]
+odd_number=[]
+for i in number:
+    if i%2!=0:
+        odd_number.append(i)
+print(odd_number)
